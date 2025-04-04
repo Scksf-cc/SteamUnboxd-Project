469db3a5ef6bbf52b68a527db0c0c4b4a9080944
John Benedict Alianza <109412441+ToshisCorner@users.noreply.github.com>
2025-04-04 23:27:38 +0400

    Update message.js

--- api/src/functions/message.js
+++ api/src/functions/message.js
@@ -20,7 +20,7 @@ async function sendAnswersToGemini() {
 
     // Send the payload to the Gemini API
     try {
-        const response = await fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-pro-exp-03-25:generateContent?key=AIzaSyC2SamwlxYOKHLImeNXYDcWqoBCitiDuTk', {
+        const response = await fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-pro-exp-03-25:generateContent?key=YOURAPIKEYHERE!!!!!', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
